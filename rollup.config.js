1ba6fd3121e7e1028e8956fb1fd1c605e5d39a68
TheJazzDev <babsman4all@gmail.com>
2025-01-21 21:07:05 +0100

    update package number

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
 import dts from "rollup-plugin-dts";
+
 export default [
   {
     input: "src/index.ts",
